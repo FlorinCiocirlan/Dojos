4e262a401b1ba2cdfa9678a56a79f6cbdc40e1d5
florineless@gmail.com <florineless@gmail.com>
2019-11-19 13:45:52 +0200

    exercise 3 file hadnling

---
+++ error_handling_exercise_2.py
@@ -0,0 +1,17 @@
+def add_to_list_in_dict(thedict, listname, element):
+    try:
+        if listname in thedict:
+            l = thedict[listname]
+            print("%s already has %d elements." % (listname, len(l)))
+        else:
+            thedict[listname] = []
+            print("Created %s." % listname)
+
+    except KeyError:
+        print("your list doesn't exist yet")
+    else:
+        thedict[listname].append(element)
+    finally:
+        print("Thanks")
+
+    print("Added %s to %s." % (element, listname))
